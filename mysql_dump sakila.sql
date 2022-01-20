4945e450c7ff38eb24444c36fe03332838207009
Ogooluwatomiwa <96211056+Ogooluwatomiwa@users.noreply.github.com>
2022-01-20 18:25:02 +0800

    Add files via upload

---
+++ mysql_dump sakila.sql
@@ -0,0 +1,11 @@
+USE sakila;
+SELECT * FROM actor;
+SELECT * FROM film;
+SELECT * FROM customer;
+SELECT title FROM film;
+SELECT DISTINCT name as language FROM language;
+SELECT COUNT(store_id) FROM store;
+SELECT COUNT(staff_id) FROM staff;
+select first_name from staff;
+
+-- this is comment 
